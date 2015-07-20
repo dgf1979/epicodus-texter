11ac16db0f773db4fcf4b471f3becf356384f8f4
Drew Finstrom & cleri philiastides <student@epicodus.com>
2015-07-20 16:53:27 -0700

    update readme

--- texter/app/controllers/replies_controller.rb
+++ texter/app/controllers/replies_controller.rb
@@ -1,9 +1,6 @@
 class RepliesController < ApplicationController
   skip_before_filter :verify_authenticity_token
-
-  def index
-
-
+  def default
     render plain: "This number isn't read by anybody - so your message about '#{params[:Body]}' has gone into a black hole."
   end
 end
