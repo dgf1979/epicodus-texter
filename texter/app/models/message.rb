ae7d73d40c1f47f96287a2e8abafb4acc8eef9b4
Drew Finstrom & cleri philiastides <student@epicodus.com>
2015-07-20 11:35:51 -0700

    add environment variables for sensitive auth data

--- texter/app/models/message.rb
+++ texter/app/models/message.rb
@@ -5,9 +5,9 @@ private
   def send_sms
    request = RestClient::Request.new(
       :method => :post,
-      :url => 'https://api.twilio.com/2010-04-01/Accounts/AC12e6b7b59a4d1103d157293cc4066466/Messages.json',
-      :user => 'AC12e6b7b59a4d1103d157293cc4066466',
-      :password => 'b66ed16455c1fb7dd67af655933d9b19',
+      :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
       :payload => { :Body => body,
                     :To => to,
                     :From => from }
